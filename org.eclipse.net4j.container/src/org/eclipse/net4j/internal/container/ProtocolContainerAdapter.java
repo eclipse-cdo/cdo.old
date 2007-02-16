562124d4e8d2b2062b4ae8f8a28f415d95e8f27a
Eike Stepper <stepper@esc-net.de>
2007-02-16 09:36:56 +0000

    *** empty log message ***

---
+++ org.eclipse.net4j.container/src/org/eclipse/net4j/internal/container/ProtocolContainerAdapter.java
@@ -0,0 +1,41 @@
+/***************************************************************************
+ * Copyright (c) 2004-2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.net4j.internal.container;
+
+import org.eclipse.net4j.container.Container;
+import org.eclipse.net4j.transport.ProtocolFactory;
+
+public abstract class ProtocolContainerAdapter extends AbstractContainerAdapter
+{
+  private ProtocolFactory protocolFactory;
+
+  protected ProtocolContainerAdapter(Container container, String type)
+  {
+    super(container, type);
+    protocolFactory = createProtocolFactory();
+  }
+
+  @Override
+  protected void onActivate() throws Exception
+  {
+    super.onActivate();
+    getContainer().register(protocolFactory);
+  }
+
+  @Override
+  protected void onDeactivate() throws Exception
+  {
+    getContainer().deregister(protocolFactory);
+    super.onDeactivate();
+  }
+
+  protected abstract ProtocolFactory createProtocolFactory();
+}
