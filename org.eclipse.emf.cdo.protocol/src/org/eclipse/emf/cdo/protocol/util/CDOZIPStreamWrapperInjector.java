6b4c501647d3d1b098a0332eb93e7836623c2663
Eike Stepper <stepper@esc-net.de>
2007-08-27 07:38:23 +0000

    *** empty log message ***

---
+++ org.eclipse.emf.cdo.protocol/src/org/eclipse/emf/cdo/protocol/util/CDOZIPStreamWrapperInjector.java
@@ -0,0 +1,26 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2007 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.emf.cdo.protocol.util;
+
+import org.eclipse.emf.cdo.protocol.CDOProtocolConstants;
+
+import org.eclipse.net4j.signal.ZIPStreamWrapperInjector;
+
+/**
+ * @author Eike Stepper
+ */
+public class CDOZIPStreamWrapperInjector extends ZIPStreamWrapperInjector
+{
+  public CDOZIPStreamWrapperInjector()
+  {
+    super(CDOProtocolConstants.PROTOCOL_NAME);
+  }
+}
