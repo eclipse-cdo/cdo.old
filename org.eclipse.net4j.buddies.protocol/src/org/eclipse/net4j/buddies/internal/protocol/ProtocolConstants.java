ea7e76962926e3cfaa1f68d939e5702b692de665
Eike Stepper <stepper@esc-net.de>
2007-10-09 12:14:29 +0000

    [205668] Provide buddies example https://bugs.eclipse.org/bugs/show_bug.cgi?id=205668

--- org.eclipse.net4j.buddies.protocol/src/org/eclipse/net4j/buddies/internal/protocol/ProtocolConstants.java
+++ org.eclipse.net4j.buddies.protocol/src/org/eclipse/net4j/buddies/internal/protocol/ProtocolConstants.java
@@ -29,9 +29,11 @@ public interface ProtocolConstants
 
   public static final short SIGNAL_INITIATE_COLLABORATION = 6;
 
-  public static final short SIGNAL_INSTALL_FACILITY = 7;
+  public static final short SIGNAL_COLLABORATION_INITIATED = 7;
 
-  public static final short SIGNAL_MESSAGE = 8;
+  public static final short SIGNAL_INSTALL_FACILITY = 8;
+
+  public static final short SIGNAL_MESSAGE = 9;
 
   public static final byte STATE_AVAILABLE = 1;
 
