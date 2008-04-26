31d7e77e1f9001b7e61bbcd3d99687dc6943717d
Eike Stepper <stepper@esc-net.de>
2008-04-26 15:26:15 +0000

    [228994]  Add a Spring config example https://bugs.eclipse.org/bugs/show_bug.cgi?id=228994

---
+++ org.eclipse.emf.cdo.examples.spring/src/org/eclipse/emf/cdo/examples/spring/Main.java
@@ -0,0 +1,41 @@
+/***************************************************************************
+ * Copyright (c) 2004 - 2008 Eike Stepper, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Eike Stepper - initial API and implementation
+ **************************************************************************/
+package org.eclipse.emf.cdo.examples.spring;
+
+import org.eclipse.net4j.internal.util.om.log.PrintLogHandler;
+import org.eclipse.net4j.internal.util.om.trace.PrintTraceHandler;
+import org.eclipse.net4j.util.om.OMPlatform;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Eike Stepper
+ */
+public class Main
+{
+  private static final String[] CONFIG_FILES = { "net4j.xml", "cdo.xml" };
+
+  public static void main(String[] args) throws Exception
+  {
+    OMPlatform.INSTANCE.setDebugging(true);
+    OMPlatform.INSTANCE.addLogHandler(PrintLogHandler.CONSOLE);
+    OMPlatform.INSTANCE.addTraceHandler(PrintTraceHandler.CONSOLE);
+
+    ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_FILES);
+    while (System.in.available() == 0)
+    {
+      Thread.sleep(200);
+    }
+
+    context.close();
+  }
+}
