350644ab4ae4cd8b1ca2bdeddfc8c6a0e9ea582d
Eike Stepper <stepper@esc-net.de>
2009-03-15 08:40:25 +0000

    [265435]  Replace CDOModelElements by EModelElements https://bugs.eclipse.org/bugs/show_bug.cgi?id=265435

--- org.eclipse.emf.cdo.tests.model2.edit/src/org/eclipse/emf/cdo/tests/model2/provider/TaskContainerItemProvider.java
+++ org.eclipse.emf.cdo.tests.model2.edit/src/org/eclipse/emf/cdo/tests/model2/provider/TaskContainerItemProvider.java
@@ -21,10 +21,15 @@ import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.IChildCreationExtender;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemColorProvider;
+import org.eclipse.emf.edit.provider.IItemFontProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITableItemColorProvider;
+import org.eclipse.emf.edit.provider.ITableItemFontProvider;
+import org.eclipse.emf.edit.provider.ITableItemLabelProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
@@ -38,7 +43,8 @@ import java.util.List;
  * @generated
  */
 public class TaskContainerItemProvider extends CDOItemProviderAdapter implements IEditingDomainItemProvider,
-    IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource
+    IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource,
+    ITableItemLabelProvider, ITableItemColorProvider, ITableItemFontProvider, IItemColorProvider, IItemFontProvider
 {
   /**
    * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
@@ -97,6 +103,17 @@ public class TaskContainerItemProvider extends CDOItemProviderAdapter implements
     // adding (see {@link AddCommand}) it as a child.
 
     return super.getChildFeature(object, child);
+  }
+
+  /**
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  public boolean hasChildren(Object object)
+  {
+    return hasChildren(object, true);
   }
 
   /**
--- org.eclipse.emf.cdo.tests.model2.edit/src/org/eclipse/emf/cdo/tests/model2/provider/TaskItemProvider.java
+++ org.eclipse.emf.cdo.tests.model2.edit/src/org/eclipse/emf/cdo/tests/model2/provider/TaskItemProvider.java
@@ -20,10 +20,15 @@ import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IChildCreationExtender;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemColorProvider;
+import org.eclipse.emf.edit.provider.IItemFontProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITableItemColorProvider;
+import org.eclipse.emf.edit.provider.ITableItemFontProvider;
+import org.eclipse.emf.edit.provider.ITableItemLabelProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
@@ -38,7 +43,8 @@ import java.util.List;
  * @generated
  */
 public class TaskItemProvider extends CDOItemProviderAdapter implements IEditingDomainItemProvider,
-    IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource
+    IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource,
+    ITableItemLabelProvider, ITableItemColorProvider, ITableItemFontProvider, IItemColorProvider, IItemFontProvider
 {
   /**
    * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
@@ -93,6 +99,17 @@ public class TaskItemProvider extends CDOItemProviderAdapter implements IEditing
         .getRootAdapterFactory(), getResourceLocator(), getString("_UI_Task_done_feature"), getString(
         "_UI_PropertyDescriptor_description", "_UI_Task_done_feature", "_UI_Task_type"),
         Model2Package.Literals.TASK__DONE, true, false, false, ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE, null, null));
+  }
+
+  /**
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
+  @Override
+  public boolean hasChildren(Object object)
+  {
+    return hasChildren(object, true);
   }
 
   /**
