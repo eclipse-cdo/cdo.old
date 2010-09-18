bdef7794231864a497a9687d5835773474cab4b4
Eike Stepper <stepper@esc-net.de>
2010-09-18 05:38:37 +0000

    *** empty log message ***

--- org.eclipse.net4j.tools.workingset.ui/src/org/eclipse/net4j/tools/workingset/ui/preferences/WorkingSetPreferencePage.java
+++ org.eclipse.net4j.tools.workingset.ui/src/org/eclipse/net4j/tools/workingset/ui/preferences/WorkingSetPreferencePage.java
@@ -10,13 +10,30 @@
  */
 package org.eclipse.net4j.tools.workingset.ui.preferences;
 
+import org.eclipse.net4j.tools.workingset.ui.embedded.EmbeddedXtextEditor;
+import org.eclipse.net4j.tools.workingset.ui.internal.DslActivator;
+
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.IAnnotationModelListener;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.model.IXtextModelListener;
+import org.eclipse.xtext.ui.editor.validation.XtextAnnotation;
 
-public class WorkingSetPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage,
+import com.google.inject.Injector;
+
+import java.util.Iterator;
+
+public class WorkingSetPreferencePage extends PreferencePage implements IWorkbenchPreferencePage,
     IWorkbenchPropertyPage
 {
   private IWorkbench workbench;
@@ -48,8 +65,54 @@ public class WorkingSetPreferencePage extends FieldEditorPreferencePage implemen
   }
 
   @Override
-  protected void createFieldEditors()
+  protected Control createContents(Composite parent)
   {
+    Composite contents = new Composite(parent, SWT.NONE);
+    contents.setLayout(new GridLayout());
+    contents.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
+    setTitle("Net4j Tools WorkingSet");
+    setMessage("Enter an expression.");
+
+    Injector injector = DslActivator.getInstance().getInjector("org.eclipse.net4j.tools.workingset.Dsl");
+
+    EmbeddedXtextEditor editor = new EmbeddedXtextEditor(contents, injector, SWT.BORDER);
+    editor.getViewer().getAnnotationModel().addAnnotationModelListener(new IAnnotationModelListener()
+    {
+      private String errorMessage;
+
+      public void modelChanged(IAnnotationModel model)
+      {
+        String text = "";
+        Iterator<?> it = model.getAnnotationIterator();
+        if (it.hasNext())
+        {
+          XtextAnnotation annotation = (XtextAnnotation)it.next();
+          text = annotation.getText();
+        }
+
+        if (!text.equals(errorMessage))
+        {
+          errorMessage = text.length() == 0 ? null : text;
+          workbench.getDisplay().asyncExec(new Runnable()
+          {
+            public void run()
+            {
+              setErrorMessage(errorMessage);
+            }
+          });
+        }
+      }
+    });
+
+    editor.getDocument().addModelListener(new IXtextModelListener()
+    {
+      public void modelChanged(XtextResource resource)
+      {
+        System.out.println(resource.getContents());
+      }
+    });
+
+    return contents;
   }
 }
