07a22ec18d4e5c2e658f70e9e2d849ef8b4ea950
Eike Stepper <stepper@esc-net.de>
2010-09-19 06:16:38 +0000

    *** empty log message ***

--- org.eclipse.net4j.tools.workingset.ui/src/org/eclipse/net4j/tools/workingset/ui/DynamicWorkingSetUpdater.java
+++ org.eclipse.net4j.tools.workingset.ui/src/org/eclipse/net4j/tools/workingset/ui/DynamicWorkingSetUpdater.java
@@ -1,5 +1,6 @@
 package org.eclipse.net4j.tools.workingset.ui;
 
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.IWorkingSetUpdater;
 
@@ -25,13 +26,18 @@ public class DynamicWorkingSetUpdater implements IWorkingSetUpdater
 
   public void add(IWorkingSet workingSet)
   {
-    System.out.println("add: " + workingSet);
+    System.out.println("add: " + workingSet.getName());
+    for (IAdaptable element : workingSet.getElements())
+    {
+      System.out.println("  " + element);
+    }
+
     workingSets.add(workingSet);
   }
 
   public boolean remove(IWorkingSet workingSet)
   {
-    System.out.println("remove: " + workingSet);
+    System.out.println("remove: " + workingSet.getName());
     return workingSets.remove(workingSet);
   }
 }
